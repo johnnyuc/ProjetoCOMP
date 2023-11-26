903868eae1072759eee4391c26ebc30eef13c8c2
Stefanos Panagoulias <118118425+StefanosDEI@users.noreply.github.com>
2023-11-26 20:59:34 +0000

    Update ast.h

--- projeto/ast.h
+++ projeto/ast.h
@@ -2,24 +2,24 @@
 #define _AST_H
 
 enum category { Program, Declaration, FuncDeclaration, FuncDefinition, ParamList, FuncBody, ParamDeclaration, StatList, If, While, Return, Or, And, Eq, Ne, Lt, Gt, Le, Ge, Add, Sub, Mul, Div, Mod, Not, Minus, Plus, Store, Comma, Call, BitWiseAnd, BitWiseXor, BitWiseOr, Char, ChrLit, Identifier, Int, Short, Natural, Double, Decimal, Void, Null };
+
 #define names { "Program", "Declaration", "FuncDeclaration", "FuncDefinition", "ParamList", "FuncBody", "ParamDeclaration", "StatList", "If", "While", "Return", "Or", "And", "Eq", "Ne", "Lt", "Gt", "Le", "Ge", "Add", "Sub", "Mul", "Div", "Mod", "Not", "Minus", "Plus", "Store", "Comma", "Call", "BitWiseAnd", "BitWiseXor", "BitWiseOr", "Char", "ChrLit", "Identifier", "Int", "Short", "Natural", "Double", "Decimal", "Void", "Null" }
 
-enum type { integer_type, short_type, double_type, char_type, void_type, no_type };
-#define type_name(type) ( \
-    (type == integer_type) ? "int" : \
-    ((type == short_type) ? "short" : \
-    ((type == double_type) ? "double" : \
-    ((type == char_type) ? "char" : \
-    ((type == void_type) ? "void" : \
-    "none")))))
+enum type {integer_type, double_type, void_type, char_type, short_type, no_type};
 
-#define category_type(category) (\
-    (category == Int) ? integer_type : \
-    ((category == Short) ? short_type : \
-    ((category == Double) ? double_type : \
-    ((category == Char) ? char_type : \
-    ((category == Void) ? void_type : \
-    no_type)))))
+#define type_name(type) \
+    (type == integer_type ? "int" : \
+     (type == double_type ? "double" : \
+     (type == void_type ? "void" : \
+     (type == char_type ? "char" : \
+     (type == short_type ? "short" : "none")))))
+
+#define category_type(category) \
+    (category == Int ? integer_type : \
+     (category == Double ? double_type : \
+     (category == Void ? void_type : \
+     (category == Char ? char_type : \
+     (category == Short ? short_type : no_type)))))
 
 struct node {
     enum category category;
@@ -30,10 +30,12 @@ struct node {
     struct node_list *brothers;
 };
 
+
 struct node_list {
     struct node *node;
     struct node_list *next;
 };
+
 
 struct node *newnode(enum category category, char *token);
 void addchild(struct node *parent, struct node *child);
@@ -43,6 +45,6 @@ int countchildren(struct node *node);
 int countbrother(struct node *node);
 void show(struct node *root, int depth);
 void deallocate(struct node *root);
+void append_brothers_to_children(struct node *parent, struct node_list *brothers);
 
-void JohnnyTree(char* parent, char* type, struct node *root);
 #endif
